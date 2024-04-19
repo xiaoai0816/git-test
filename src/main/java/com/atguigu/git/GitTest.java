0056f572315961660ed8c0afe81d4a82cfa1cdd6
xiaoai0816 <151721488+xiaoai0816@users.noreply.github.com>
2024-04-19 22:22:51 +0800

    pull commit2

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -10,5 +10,6 @@ public class GitTest {
         System.out.println("master test!");
         System.out.println("push test!");
         System.out.println("push2 test!");
+        System.out.println("pull test!");
     }
 }
